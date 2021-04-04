b67ef47da3c8dc024bd5227d20ae613369aa92ac
Balázs Benedek <benedekabalazs@gmail.com>
2021-04-04 11:30:15 +0300

    Create Gradle Project

---
+++ moodle/moodle-server/src/test/java/src/main/java/dev/blasio99/moodle/MoodleApplicationTests.java
@@ -0,0 +1,13 @@
+package src.main.java.dev.blasio99.moodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
